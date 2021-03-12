f7e35662dec21b6212c6a53090616d7d2d263ac5
Vlad Rakut <mentos28059@gmail.com>
2021-03-12 15:44:42 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,35 @@
-
 // You should implement your task here.
-
+ 
+// Сортировка чисел по возрастанию 
+ 
+function great(a,b) {
+  if (a > b) {
+    return 1;
+  }
+}
+ 
+// Сортировка по убыванию 
+ 
+function less(a,b) {
+  if (a < b) {
+    return 1;
+  }
+}
+ 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix == undefined) {
+    return [];
+  }
+  let resArr = [];
+  for(let i =0; i < matrix.length; i++) {
+    let cmp = great;
+    if (i % 2 != 0) {
+      cmp = less;
+    }
+    matrix[i].sort(cmp);
+    for(let j = 0; j < matrix[i].length; j++) {
+      resArr.push(matrix[i][j]);
+    }
+  }
+  return resArr;
 }
